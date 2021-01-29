f61d9d5b5f2dc8592fe6d3734ec71cf4225d856b
SuperDirtEater64 <78036481+SuperDirtEater64@users.noreply.github.com>
2021-01-28 20:10:37 -0500

    Update Solution 1.cpp

--- Solution 1.cpp
+++ Solution 1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#using namespace std;
+#include <vector>
+using namespace std;
 
 int main()
 {
+        cout << "begin\n";
+
         //A and B are the variables for temporarily holding the values of the numbers to be checked.
         int A = 0;
         int B = 0;
@@ -12,35 +15,57 @@ int main()
         //Sol is the solution to the problem, found by multiplying the two correct numbers.
         int Sol = 0;
 
+        cout << "var intiialized\n";
+
         //reading from the text file to input stream
         ifstream is("input.txt");  
-        string line;     
+        string line;
+
+        cout << "file opened\n";
+
+        //creating a vector from the input stream
+        vector<int> dataList;
+        dataList.clear();
+
+        cout << "vector created\n";
 
         //Let's start iterating through the list
         if (is.is_open())
         {
             for (line; getline(is, line);)
             {
-                A = stoi (line);
-                
+                dataList.push_back(stoi(line));
+            }
 
-                for (line; getline(is, line);)
+            //A Loop
+            for (int i = 0; i < dataList.size(); i++)
+            {
+                A = dataList[i];
+                cout << "A: " << A << "\n";
+
+                //B Loop
+                for (int j = 0; j < dataList.size(); j++)
                 {
-                    B = stoi(line);
-                    Sol = A * B;
+                    B = dataList[j];
+                    cout << "B: " << B << "\n";
+
+                   Sol = A * B;
 
                     if (A + B == 2020 && A != B)
                     {
                         //answer
-                        cout << "The two numbers are " << A << " and " << B << " and their product is " << Sol << "/n";
+                        cout << "The two numbers are " << A << " and " << B << " and their product is " << Sol << "\n";
                         break;                       
                     }
                 }
-
+                
                 if (A + B == 2020)
                 {
                     break;
                 }
-            }    
-        }
+
+            }
+
+            
+        }    
 }
